22c53e8c2f6a7bcbcf37bb812ebca97f135b0b82
Alvaro Lima <120517164+AlvaroLima80@users.noreply.github.com>
2024-08-07 10:06:11 -0300

    VETOR 1
    Programa simples de vetor, representando um programa bruto, onde os valores das posições do vetor já são definidas previamente.

---
+++ VETOR.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	
+	int v[5];
+	int soma;
+	
+	v[0]= 10;
+	v[1]= 20;
+	v[2]= 30;
+	v[3]= 40;
+	v[4]= 50;
+	
+	soma = (v[0]+v[1]+v[2]+v[3]+v[4]);
+	
+	printf("A soma dos vetores foi: %d ", soma);
+	
+}
